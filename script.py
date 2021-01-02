9ae0f1bee5f0d1cc0499afbe20136fa7aaf6261f
Jonathan Adriano <jadriano@est.ups.edu.ec>
2021-01-01 21:54:52 -0500

    Commit inicial

---
+++ script.py
@@ -0,0 +1,10 @@
+# Script de prueba para Git
+# Jonathan Adriano <jadriano@ups.edu.ec> # Fecha: 01/01/2021
+# Versión: 1.0.0
+
+def main():
+    print("Hola Mundo!")
+
+
+if  name 	== " main ": 
+    main()
